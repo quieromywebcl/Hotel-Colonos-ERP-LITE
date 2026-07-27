983cfe5322c335cf17f1359eee916431bda4fcb4
quieromywebcl <quieromyweb.cl@gmail.com>
2026-07-27 07:20:58 -0400

    Delete src directory

--- src/lib/supabase.ts
+++
@@ -1,4 +0,0 @@
-import { supabase, SUPABASE_URL, SUPABASE_ANON_KEY, isSupabaseConfigured } from './supabaseClient';
-
-export { supabase, SUPABASE_URL, SUPABASE_ANON_KEY, isSupabaseConfigured };
-export default supabase;
